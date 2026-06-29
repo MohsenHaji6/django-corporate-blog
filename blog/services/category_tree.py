23175efceb85654c67ead4f2ff3d7e2187e6ec60
Mohsen Hajizadeh <m.hajizadeh2023@gmail.com>
2026-06-29 23:22:38 +0330

    Implement tree structure for blog category

---
+++ blog/services/category_tree.py
@@ -0,0 +1,35 @@
+from blog.models import Category
+
+
+def build_category_tree():
+
+    categories = Category.objects.only(
+        "id",
+        "name",
+        "slug",
+        "path",
+        "depth",
+        "numchild",
+    )
+
+    tree = []
+    lookup = {}
+    for category in categories:
+        node = {
+            "pk": category.pk,
+            "name": category.name,
+            "slug": category.slug,
+            "depth": category.depth,
+            "url": category.get_absolute_url(),
+            "has_children": category.numchild > 0,
+            "children": [],
+        }
+        lookup[category.path] = node
+
+        if category.depth == 1:
+            tree.append(node)
+        else:
+            parent_path = category.path[: -Category.steplen]
+            lookup[parent_path]["children"].append(node)
+
+    return tree
