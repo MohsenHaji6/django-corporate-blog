b4b1102e226088a80112e1943233f4322d20b085
Mohsen Hajizadeh <m.hajizadeh2023@gmail.com>
2026-08-30 12:51:13 +0330

    Debug entrypoint.sh for render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "=== ENTRYPOINT STARTED ==="
+
+python manage.py migrate --noinput
+
+echo "=== MIGRATE FINISHED ==="
+
 python manage.py collectstatic --noinput
+
 echo "=== COLLECTSTATIC FINISHED ==="
 exec "$@"
